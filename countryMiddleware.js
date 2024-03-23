d8e72e4c8f705654a1b0f5972904df7730646d44
marwanzayed16 <marwanzayed16@gmail.com>
2024-03-23 21:33:56 +0200

    Upload Files

--- countryMiddleware.js
+++ countryMiddleware.js
@@ -1,8 +1,15 @@
+// countryMiddleware.js
 const axios = require('axios');
 
 async function getCountryFromIP(req, res, next) {
     try {
-        const response = await axios.get(`https://ipinfo.io/country`);
+        // Get client IP address from request object
+        const ipAddress = req.ip;
+
+        // Make a request to ipinfo.io to get country information based on the IP address
+        const response = await axios.get(`https://ipinfo.io/${ipAddress}/country`);
+
+        // Extract country from the response data
         req.country = response.data;
         next();
     } catch (error) {
